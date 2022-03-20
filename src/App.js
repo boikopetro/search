7c4a8359203b88e1463b2051d13e59847f5d5150
boikopetro <boikopetromail@gmail.com>
2022-03-20 17:56:17 +0200

    change search

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import {useMemo, useState} from "react";
 function App() {
   const data = ["вс2249аа", "ав12345ав", "аа456аа"];
   const [searchQuery, setSearchQuery] = useState("");
-
   const search = useMemo(() => data.filter(n => n.trim().toUpperCase().includes(searchQuery.trim().toUpperCase()))
       , [searchQuery,data])
-  console.log(search)
   return (
       <div className="App">
         <h1>Пошук</h1>
@@ -21,8 +19,8 @@ function App() {
         <div>
           {searchQuery && search.map(n =>
               <div>
-                <p key={n.i} style={{color: (n === searchQuery) ? "red" : "black"}}>{n}</p>
-                <div>{(n === searchQuery) ? <p className="danger">Розшук!!!</p> : ""}</div>
+                <p key={n.i} style={{color: (n.trim().toUpperCase() === searchQuery.trim().toUpperCase()) ? "red" : "black"}}>{n}</p>
+                <div>{(n.trim().toUpperCase() === searchQuery.trim().toUpperCase()) ? <p className="danger">Розшук!!!</p> : ""}</div>
               </div>)}
         </div>
       </div>
